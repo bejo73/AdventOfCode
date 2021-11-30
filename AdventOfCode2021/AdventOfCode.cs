15913092463e374e6431d957f15ce45962d4b9e8
Jörgen Bergström <jorgen@bergstrom.nu>
2021-11-30 16:10:08 +0100

    Preparations for 2021

---
+++ AdventOfCode2021/AdventOfCode.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Day01.Run();
+//Day99.Run();
+
+Console.WriteLine("Merry Christmas...");
+Console.ReadKey();
